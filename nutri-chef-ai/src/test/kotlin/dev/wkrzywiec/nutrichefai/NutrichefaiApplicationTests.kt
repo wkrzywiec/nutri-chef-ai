7c105ef6ce603a84baf793113e488f0510d2bd68
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2025-08-11 07:22:01 +0200

    add nutri-chef-ai service

---
+++ nutri-chef-ai/src/test/kotlin/dev/wkrzywiec/nutrichefai/NutrichefaiApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.wkrzywiec.nutrichefai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NutrichefaiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
